6b9815c8410fffa456da1d0b80d11f94299167d1
Felipe Martins <lipefenix2@gmail.com>
2023-10-04 20:37:07 -0300

    Heroku files

--- django02/settings.py
+++ django02/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 #usando postegre SQL COM heroku
-databases = {
+DATABASES = {
     'default':dj_database_url.config()
 }
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
